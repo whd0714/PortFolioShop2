4a1271e3d69f91f82f7b0f5ce5d610d4f45e0c79
whd07 <whd0714@naver.com>
2021-03-24 19:24:16 +0900

    Initial commit

---
+++ src/main/java/portfolioshop/PortFolioShopApplication.java
@@ -0,0 +1,13 @@
+package portfolioshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortFolioShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortFolioShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/portfolioshop/PortFolioShopApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolioshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortFolioShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
